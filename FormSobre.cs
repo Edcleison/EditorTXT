5cf7bfef6f50b35a00c09187a760d3b808b0d308
Edcleison <edcleisonbrito@gmail.com>
2022-07-22 22:31:53 -0300

    Initial commit

---
+++ FormSobre.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EditorTXT
+{
+    public partial class FormSobre : Form
+    {
+        public FormSobre()
+        {
+            InitializeComponent();
+
+            WebBrowser webSobre = new WebBrowser();
+            webSobre.Dock = DockStyle.Fill;
+            webSobre.Navigate(Application.StartupPath + @"sobre\index.html");
+            this.Controls.Add(webSobre);
+        }
+    }
+}
---
+++ Gerenciador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditorTXT
+{
+    public static class Gerenciador
+    {
+        //Valores Padrão
+        public static string DefautFolderPath => Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\";
+
+        public static string DefautFileName => "arquivo";
+        public static string DefautFileExt => ".txt";
+
+        // Valores Atribuidos ao uso
+        public static string FolderPath { get; set; } = DefautFolderPath;
+        public static string FileName { get; set; } = DefautFileName;
+        public static string FileExt { get; set; } = DefautFileExt;
+        public static string FilePath => FolderPath + FileName + FileExt;
+
+    }
+}
